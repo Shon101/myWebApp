e4b7603a81663152c8291a2ae9f904a13394d995
shon101 <neokar2010@gmail.com>
2012-12-20 19:53:33 -0800

    sandalone code added

--- assets/includes/esales_aid.js
+++ assets/includes/esales_aid.js
@@ -33,6 +33,7 @@ $(document).ready(function(){
 	window.setTimeout('startMap()',3000);
 	
 	checkDevice();
+	checkiPadStandAlone();
 });
 
 
@@ -93,5 +94,11 @@ function checkDevice(){
 	}else{
 		$('.page').css('display','none');
 		$('body').css('background-color','#fff').append('<a href="mailto:?subject=Check%20out%20this%20eSales%20Aid%20Web%20App%20for%20iPad&amp;body=Add%20this%20Web%20App%20to%20your%20iPad%20by%20visiting:%20http://codifydesign.com/chris/lynda/samples/course-0010/"><img src="assets/images/template/non_ipad_message.png?v=1"/></a>');
+	}
+}
+function checkiPadStandAlone(){
+	if(window.navigator.standalone == false) {
+		$('.page').css('display','none');
+		$('body').css('background-image','url(assets/images/template/background_content_home.jpg?v=1)').append('<img  src="assets/images/template/add_to_homescreen.png?v=1"/>');
 	}
 }
